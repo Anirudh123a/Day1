fe6007a0065edf0703654b0690150c0f303670b8
Anirudhan <anirudhjb2003@gmail.com>
2025-08-13 14:59:29 +0530

    Delete DAY8 directory

--- DAY8/new.js
+++
@@ -1,9 +0,0 @@
-function printText(text)
-{
-    console.log("the text given is :"+text);
-}
-let arr=["class Room","car","Bike","CAB","BAT"];
-for(let i=arr.length-1;i>=0;i--)
-{
-    printText(arr[i]);
-}
